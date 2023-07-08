b79d826baa75fc441e2b4f254ac15cecb8596791
Nahuel Romero <nnromero05@gmail.com>
2023-07-08 02:05:39 -0300

    cambios leves para github del registro.php

--- PHP/registro.php
+++ PHP/registro.php
@@ -6,21 +6,21 @@ if (isset($_POST['nombre']) && isset($_POST['usuario']) && isset($_POST['passwor
     $unique_user = $_POST['usuario'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
-    // check if unique user exists
+    // checa que el usuario no exista
     $sql = "SELECT * FROM usuarios WHERE usuario='$unique_user'";
     $result = mysqli_query($conn, $sql);
-    // if user exists, display error message
+    // Si el usuario existe da este mensaje de error
     if (mysqli_num_rows($result) > 0) {
         echo '<p>Error: This user already exists.</p>';
     } else {
-        // check if passwords match
+        // checa que las contraseñas sean iguales
         if ($password != $confirm_password) {
             echo '<p>Error: Passwords do not match.</p>';
         } else {
-            // insert user into database
+            // Inserta los datos a la bd
             $sql = "INSERT INTO usuarios (nombre, usuario, password, tipoDeUsuario) VALUES ('$username', '$unique_user', '$password', 1)";
             mysqli_query($conn, $sql);
-            // redirect to index.html
+            // redirecciona a el index
             header('Location: ../index.html');
         }
     }
