e010b28d930fb1e8c4b20f1221a2c02fd18db604
Nahuel Romero <nnromero05@gmail.com>
2023-09-12 21:02:30 -0300

    Cambio de rutas en app.js

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const path = require('path'); // Importa el módulo 'path' para manejar rutas de archivos
+const path = require('path');
 const app = express();
 const port = 3000;
-const mysql = require('mysql2/promise'); // Importa mysql2/promise
-
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 const sessionSecretKey = process.env.SESSION_SECRET;
 
-// Configuración de sesiones
 app.use(session({
-  secret: sessionSecretKey, // Cambia esto a una cadena segura
+  secret: sessionSecretKey,
   resave: false,
   saveUninitialized: true
 }));
 
-// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Importa y utiliza el enrutador
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-// Enrutamiento para /panel después de la autenticación
+// Redirige desde la raíz a /login
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
 app.use('/panel', (req, res, next) => {
   if (req.session.logged_in) {
-    const panelRouter = require('./routes/panel'); // Importa el enrutador panel.js
+    const panelRouter = require('./routes/panel');
     app.use('/panel', panelRouter);
     next();
   } else {
-    // Si el usuario no está autenticado, redirige a la página de inicio de sesión
     res.redirect('/');
   }
 });
