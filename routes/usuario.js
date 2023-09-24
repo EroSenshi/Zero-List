a982f8214132aa8872d76097a30ba9e75f6464a0
Nahuel Romero <nnromero05@gmail.com>
2023-09-24 01:37:53 -0300

    Cambios rotundos en el css en general menos en el del Panel

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,12 +4,12 @@ const router = express.Router();
 // Ruta para obtener el tipo de usuario actual
 router.get('/tipo-usuario', (req, res) => {
     if (req.session.loggedIn) {
-      // Si el usuario está autenticado, envía el tipo de usuario almacenado en la sesión
-      res.json({ tipoDeUsuario: req.session.tipoDeUsuario });
+        // Si el usuario está autenticado, envía el tipo de usuario y el ID del usuario almacenados en la sesión
+        res.json({ tipoDeUsuario: req.session.tipoDeUsuario, id: req.session.id });
     } else {
-      // Si el usuario no está autenticado, envía un valor predeterminado o un código de error
-      res.json({ tipoDeUsuario: -1 }); // Puedes definir otro valor si lo prefieres
+        // Si el usuario no está autenticado, envía un valor predeterminado o un código de error
+        res.json({ tipoDeUsuario: -1, id: null }); // Puedes definir otro valor si lo prefieres
     }
-  });
-  
+});
+
 module.exports = router;
