a3e7acb7f437c624d93de5f665083a9291fe30b2
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-10 23:28:53 -0300

    cambios rapidos y adaptando el php
    Cambio de la variable contraseña en password, adaptando el login para la creacion de cookie y verificacion de usuario

--- src/registro.php
+++ src/registro.php
@@ -1,15 +1,12 @@
 <?php
-
-if ($conn->connect_error) {
-    die("Error en la conexión a la base de datos: " . $conn->connect_error);
-}
+include('../config/db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombreusuario = $_POST["nombreusuario"];
     $nombre = $_POST["nombre"];
     $tipo_usuario = $_POST["tipo_usuario"];
-    $contrasena = $_POST["contrasena"];
-    $confirmar_contrasena = $_POST["confirmar_contrasena"];
+    $password = $_POST["password"];
+    $confirmar_password = $_POST["confirmar_password"];
 
     // Verificar si el nombre de usuario ya existe en la base de datos
     $sql = "SELECT id FROM usuarios WHERE nombreusuario = '$nombreusuario'";
@@ -17,17 +14,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         echo "El nombre de usuario ya existe. Por favor, elige otro.";
-    } elseif ($contrasena !== $confirmar_contrasena) {
+    } elseif ($password !== $confirmar_password) {
         echo "Las contraseñas no coinciden. Inténtalo de nuevo.";
     } else {
         // Hash de la contraseña para mayor seguridad (puedes utilizar una función de hashing más segura)
-        $hashed_password = password_hash($contrasena, PASSWORD_DEFAULT);
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
         // Insertar el nuevo usuario en la base de datos
-        $sql = "INSERT INTO usuarios (nombreusuario, nombre, tipo_usuario, contrasena) VALUES ('$nombreusuario', '$nombre', '$tipo_usuario', '$hashed_password')";
+        $sql = "INSERT INTO usuarios (nombreusuario, nombre, tipo_usuario, password) VALUES ('$nombreusuario', '$nombre', '$tipo_usuario', '$hashed_password')";
         if ($conn->query($sql) === TRUE) {
             // Redireccionar a otra página después del registro
-            header("Location: otra_pagina.php");
+            header("Location: ../public/views/login.html");
             exit();
         } else {
             echo "Error al registrar el usuario: " . $conn->error;
