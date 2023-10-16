d9e64ac0f76466f672000479b728ea831cb24aea
SergioBritos <143556023+SergioBritos@users.noreply.github.com>
2023-10-16 18:03:52 -0300

    Add files via upload

--- src/delete.php
+++ src/delete.php
@@ -1,33 +1,34 @@
 <?php
 include('../../config/db.php');
-
 // Obtener el ID del curso a eliminar
 $curso_id = $_GET["id"];
 
-// Iniciar una transacción para asegurarse de que todas las eliminaciones se realicen correctamente
+// Iniciar una transacción para asegurarse de que ambas eliminaciones se realicen correctamente
 $conn->begin_transaction();
 
+// Consulta SQL para eliminar los alumnos del curso
+
+
+// Consulta SQL para eliminar el curso
+$sqlEliminarCurso = "DELETE FROM cursos WHERE id = $curso_id";
+
 try {
+    // Eliminar los alumnos del curso
 
-    // Consulta SQL para eliminar las relaciones entre alumnos y cursos
-    $sqlEliminarRelaciones = "DELETE FROM cursos_alumnos WHERE id_curso = $curso_id";
-    $conn->query($sqlEliminarRelaciones);
-
-    // Consulta SQL para eliminar el curso
-    $sqlEliminarCurso = "DELETE FROM cursos WHERE id = $curso_id";
+    // Eliminar el curso
     $conn->query($sqlEliminarCurso);
 
     // Confirmar la transacción
     $conn->commit();
 
-    echo "Curso, alumnos y asistencias eliminados correctamente.";
+    echo "Curso y alumnos eliminados correctamente.";
     header("Location: mostrar_cursos.php");
     exit;
 } catch (Exception $e) {
     // Revertir la transacción en caso de error
     $conn->rollback();
 
-    echo "Error al eliminar el curso, los alumnos y las asistencias: " . $e->getMessage();
+    echo "Error al eliminar el curso y los alumnos: " . $e->getMessage();
 }
 
 // Cerrar la conexión a la base de datos
