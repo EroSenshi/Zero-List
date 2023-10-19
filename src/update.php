5f8f04773f1dcd818be8515663697458ea32a373
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-19 19:36:04 -0300

    Add files via upload

--- src/update.php
+++ src/update.php
@@ -1,3 +1,27 @@
+<?php
+include('../config/db.php');
+$nombre="";
+if (isset($_COOKIE["user_id"])) {
+    $user_id = $_COOKIE["user_id"];
+    
+    // No es necesario incluir la conexión nuevamente aquí, ya que se incluyó arriba
+
+    // Realiza la consulta SQL para obtener el nombre del usuario
+    $query = "SELECT nombre FROM usuarios WHERE id = $user_id";
+    $result = $conn->query($query);
+    
+    // Verifica si la consulta fue exitosa
+    if ($result) {
+        // Obtiene el resultado de la consulta
+        $row = $result->fetch_assoc();
+        
+        // El nombre del usuario se encuentra en $row['nombre']
+        $nombre = $row['nombre'];
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,16 +30,11 @@
     <link rel="stylesheet" href="../public/styles/create.css">
 </head>
 <body>
-<header>
-        <img src="logo.png" alt="">
-        <nav class="navigation">
-        <a href="mostrar_cursos.php"><button class="curso-boton">Panel de Curso</button></a> 
-        </nav>
-    </header>
+<nav>
+        <h2>Bienvenido <?php echo $nombre; ?></h2>
+        <a href="../public/views/mostrar_cursos.php">Panel de Cursos</a>
+    </nav>  
     <?php
-    // Conectar a la base de datos (reemplaza con tus propios datos de conexión)
-    include('../../config/db.php');
-
     // Obtener el ID del curso a editar
     $curso_id = $_GET["id"];
 
@@ -57,7 +76,5 @@
         <input class="boton" type="submit" value="Guardar Cambios">
     </form>
 </div>
-
-    <a href="mostrar_cursos.php">Volver a la Lista de Cursos</a>
 </body>
 </html>
