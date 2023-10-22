ae67e222195ce5dc87da0ed6b475bffed8397f18
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-22 16:17:34 -0300

    Update cursos_alumnos.php

--- src/cursos_alumnos.php
+++ src/cursos_alumnos.php
@@ -1,4 +1,41 @@
 <?php
+include('../config/db.php');
+$nombre="";
+if (isset($_COOKIE["user_id"])) {
+    $user_id = $_COOKIE["user_id"];
+    
+    // No es necesario incluir la conexión nuevamente aquí, ya que se incluyó arriba
+
+    // Realiza la consulta SQL para obtener el nombre del usuario
+    $query = "SELECT nombre FROM usuarios WHERE id = $user_id";
+    $result = $conn->query($query);
+    
+    // Verifica si la consulta fue exitosa
+    if ($result) {
+        // Obtiene el resultado de la consulta
+        $row = $result->fetch_assoc();
+        
+        // El nombre del usuario se encuentra en $row['nombre']
+        $nombre = $row['nombre'];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mostrar Cursos</title>
+    <link rel="stylesheet" href="../public/styles/cursols.css">
+</head>
+<body>
+    <nav>
+        <h2>Bienvenido <?php echo $nombre; ?></h2>
+        <a href="lista_cursos.php">Ver Cursos</a>
+    </nav>  
+    <h1 class="titulo">Lista de Cursos</h1>
+    <div class="carta-container">
+<?php
 
 $sql = "SELECT cursos.id, cursos.nombre_curso FROM cursos
     JOIN cursos_alumnos ON cursos.id = cursos_alumnos.id_curso
@@ -29,3 +66,6 @@ if (mysqli_num_rows($result) > 0) {
 mysqli_close($conn);
 
 ?>
+ </div>
+</body>
+</html>
