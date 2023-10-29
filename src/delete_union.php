1ce8192ade3d8274b290a0cbb542499ac1246ebc
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-29 00:37:14 -0300

    Update delete_union.php

--- src/delete_union.php
+++ src/delete_union.php
@@ -2,27 +2,25 @@
 include('../config/db.php');
 // Obtener el ID del curso a eliminar
 $curso_id = $_GET["id"];
-
+$user_id = $_COOKIE["user_id"];
 // Iniciar una transacción para asegurarse de que ambas eliminaciones se realicen correctamente
 $conn->begin_transaction();
 
-// Consulta SQL para eliminar los alumnos del curso
-
-
 // Consulta SQL para eliminar el curso
 $sqlEliminarCurso = "DELETE FROM cursos_alumnos WHERE id_curso = $curso_id";
+$sqlEliminarAsistencia = "DELETE FROM asistencia WHERE alumno_id = $user_id";
 
 try {
     // Eliminar los alumnos del curso
 
     // Eliminar el curso
     $conn->query($sqlEliminarCurso);
-
+    $conn->query($sqlEliminarAsistencia);
     // Confirmar la transacción
     $conn->commit();
 
     echo "Curso y alumnos eliminados correctamente.";
-    header("Location: cursos_alumnos.php");
+    header("Location: paneles/cursos_alumnos.php");
     exit;
 } catch (Exception $e) {
     // Revertir la transacción en caso de error
