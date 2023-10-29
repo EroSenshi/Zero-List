2fee10558139ebd225dd0d55e52eb1ad55cb382e
SomeTheodor <137733932+SomeTheodor@users.noreply.github.com>
2023-10-28 23:48:06 -0300

    Update asistencia.sql

--- bd/asistencia.sql
+++ bd/asistencia.sql
@@ -2,5 +2,6 @@ CREATE TABLE asistencia (
     id INT AUTO_INCREMENT PRIMARY KEY,
     alumno_id INT,
     fecha_hora DATETIME,
-    estado VARCHAR(255)
+    estado VARCHAR(255),
+    alumno_id INT
 );
